e7604ab913cb0590b6b28658f0dd39da0999f1c9
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-05-22 21:14:17 +0900

    Azure Deploy用の修正

--- core/production.py
+++ core/production.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+"""STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')"""
 
 
 # DB_HOST は、完全なURLではなくサーバー名のみです。
