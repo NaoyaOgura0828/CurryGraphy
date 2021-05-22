9b5a480b6a36d7d6a8af5e826b5f6a6351194076
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-05-22 15:00:21 +0900

    Azure Deploy用の修正

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import os
 
 # 次のようにプロジェクト内にパスを作成します : BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - 本番環境には不適切
