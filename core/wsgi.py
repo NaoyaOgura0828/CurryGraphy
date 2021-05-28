c45ec1f7f68bb0be432c10f234be6b46bf2614e2
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-05-28 21:14:08 +0900

    DEBUG False 時の wsgi設定

--- core/wsgi.py
+++ core/wsgi.py
@@ -13,8 +13,7 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 
-settings_module = "core.settings" if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 sys.path.append('/home/NaoyaOgura/CurryGraphy')
 
