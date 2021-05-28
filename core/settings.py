db63f9f6607fceeb3d6231a0912e4185cb9b69be
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-05-28 18:51:20 +0900

    DEBUG False 時の wsgi設定

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # セキュリティ警告 : 本番環境でデバッグをオンにして実行しないでください。
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']] if 'ALLOWED_HOSTS' in os.environ else []
 
 
 # Application definition
